0f01181415c6f710a03ec41f1e72aab80d7d2772
alswl92 <alswl92@gmail.com>
2022-10-12 17:36:20 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import router from './usersRoutes.js'
 
+const serverless = require('serverless-http');
 const app = express();
 const port = 3001; 
+
+module.exports.handler= serverless(app);
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/', router);
+app.use('/.nelify/functions/api', router);
 
 app.get('/',(req,res) => res.send("hello from express"));
 app.all("*", (req,res) => res.send("That route doesn't exist"));
