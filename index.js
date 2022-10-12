124b1b0bf06550bb179baaad4dc12f288742b976
alswl92 <alswl92@gmail.com>
2022-10-12 17:57:14 -0400

    revert netlify

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import router from './usersRoutes.js'
 
-const serverless = require('serverless-http');
 const app = express();
 const port = 3001; 
-
-module.exports.handler= serverless(app);
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/.nelify/functions/api', router);
+app.use('/', router);
 
 app.get('/',(req,res) => res.send("hello from express"));
 app.all("*", (req,res) => res.send("That route doesn't exist"));
